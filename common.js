b51b75e8fdb4423a9fea2c46f909fbbb77d9436e
Logan Su <ludi1001@gmail.com>
2015-01-17 19:58:50 -0500

    temp 2

--- common.js
+++ common.js
@@ -1,3 +1,3 @@
 // Init UserApp with your App Id
 // https://help.userapp.io/customer/portal/articles/1322336-how-do-i-find-my-app-id-
-UserApp.initialize({ appId: "54ba1a50d3ba5" });
+UserApp.initialize({ appId: "54baafbbb31af"/*"54ba1a50d3ba5"*/ });
